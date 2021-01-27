4ddc4d0401827ebfbf921de1e5ad2b53f31b3e6f
Sara-Petrovic <sarapetrovicue@gmail.com>
2021-01-27 12:03:57 +0100

    dodat meni i dugme uloguj se

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,18 @@
+export const MenuItems = [
+    {
+        title: 'Pocetna',
+        url: '/',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Kontakt',
+        url: '/contact',
+        cName: 'nav-links'
+    },{
+        title: 'Uloguj se',
+        url: '/contact',
+        cName: 'nav-links-mobile'
+    }
+   
+
+]
